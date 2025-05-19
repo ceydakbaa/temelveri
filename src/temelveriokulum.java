023f23cc276677bf86c22b6aa6ec7d2c3a7259ab
Ceyda Kaba <ceydakaba@gmail.com>
2025-05-19 21:51:30 +0300

    yaptık

---
+++ src/temelveriokulum.java
@@ -0,0 +1,34 @@
+public class temelveriokulum {
+    ;
+
+    public static void main(String[] args) {
+        // Değişken tanımlamaları ve değer atamaları
+        int i = 10;
+        System.out.println("int i: " + 10);
+
+        int i1 = 12;
+        int i2 = 81;
+        System.out.println("int i1: " + 12);
+        System.out.println("int i2: " + 81);
+
+        double ondaliksayi = 9.0;
+        System.out.println("double: " + ondaliksayi);
+
+        float kucukondalik = 3.14f;
+        System.out.println("float: " + kucukondalik);
+
+        boolean dogruMu = true;
+        System.out.println("boolean: " + dogruMu);
+
+        char karakter = 'C';
+        System.out.println("char: " + karakter);
+
+        String metin = "Merhaba Java!";
+        System.out.println("String: " + metin);
+
+        // sabit değişken
+        final int sayım;
+        sayım = 7;
+        System.out.println("final int  sayım : " + sayım);
+    }
+}
